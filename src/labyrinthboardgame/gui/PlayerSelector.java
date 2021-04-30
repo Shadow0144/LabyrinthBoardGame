db937849be6fa737f245ebdfc5a58c02f98b5fd8
Shadow0144 <CorbinCogswell@gmail.com>
2021-04-30 16:32:10 -0500

    Fixed up the menu screens

--- src/labyrinthboardgame/gui/PlayerSelector.java
+++ src/labyrinthboardgame/gui/PlayerSelector.java
@@ -5,16 +5,20 @@
  */
 package labyrinthboardgame.gui;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 /**
@@ -23,10 +27,11 @@ import javafx.scene.text.Text;
  */
 public class PlayerSelector extends VBox
 {
-    private Text nameText;
-    private Label playerTypeLabel;
+    private final TextField nameText;
+    private final Label playerTypeLabel;
     
-    private enum PlayerType {
+    private enum PlayerType
+    {
       human,
       ai,
       advanced_ai
@@ -35,37 +40,119 @@ public class PlayerSelector extends VBox
     
     /**
      * Allows selecting the player type (e.g. human or ai) for each player
+     * @param playerColor The background color for this selector
      */
-    public PlayerSelector()
+    public PlayerSelector(Color playerColor)
     {
         super();
         setAlignment(Pos.CENTER);
         
-        nameText = new Text();
+        nameText = new TextField("Player Name");
+        nameText.setAlignment(Pos.CENTER);
+        nameText.setFont(Font.font(14));
+        setMargin(nameText, new Insets(12, 12, 8, 12));
         getChildren().add(nameText);
         
         HBox selectionBox = new HBox();
         selectionBox.setAlignment(Pos.CENTER);
         
-        Button leftButton = new Button();
+        Button leftButton = new Button("<");
+        leftButton.setAlignment(Pos.CENTER);
+        selectionBox.setMargin(leftButton, new Insets(0, 12, 12, 12));
+        leftButton.setOnAction(new EventHandler<ActionEvent>() 
+        {
+            public void handle(ActionEvent event)
+            {
+                leftPressed();
+            }
+        });
+        
         playerTypeLabel = new Label();
-        Button rightButton = new Button();
+        playerTypeLabel.setAlignment(Pos.CENTER);
+        playerTypeLabel.setMinWidth(100); // Prevents the buttons from moving when we change the text
+        playerTypeLabel.setBackground(new Background(new BackgroundFill(Color.LIGHTGREY, CornerRadii.EMPTY, Insets.EMPTY)));
+        playerTypeLabel.setFont(Font.font(14));
+        selectionBox.setMargin(playerTypeLabel, new Insets(0, 12, 12, 12));
+        
+        Button rightButton = new Button(">");
+        rightButton.setAlignment(Pos.CENTER);
+        selectionBox.setMargin(rightButton, new Insets(0, 12, 12, 12));
+        rightButton.setOnAction(new EventHandler<ActionEvent>() 
+        {
+            public void handle(ActionEvent event)
+            {
+                rightPressed();
+            }
+        });
         
         playerType = PlayerType.human;
-        playerTypeLabel.setText("Human");
+        updatePlayerTypeText();
         
         selectionBox.getChildren().add(leftButton);
         selectionBox.getChildren().add(playerTypeLabel);
         selectionBox.getChildren().add(rightButton);
         getChildren().add(selectionBox);
+        
+        setMargin(this, new Insets(12, 12, 12, 12));
+        this.setBackground(new Background(new BackgroundFill(playerColor, CornerRadii.EMPTY, Insets.EMPTY)));
     }
     
     /**
-     * Sets the color for the selector
-     * @param playerColor The color for the selector
+     * Updates the text box to display what type of controller the player has
      */
-    public void setColor(Color playerColor)
+    private void updatePlayerTypeText()
     {
-        this.setBackground(new Background(new BackgroundFill(playerColor, CornerRadii.EMPTY, Insets.EMPTY)));
+        switch (playerType)
+        {
+            case human:
+                playerTypeLabel.setText("Human");
+                break;
+            case ai:
+                playerTypeLabel.setText("AI");
+                break;
+            case advanced_ai:
+                playerTypeLabel.setText("Advanced AI");
+                break;
+        }
+    }
+    
+    /**
+     * Called when the left button is pressed and changes the player type
+     */
+    private void leftPressed()
+    {
+        switch (playerType)
+        {
+            case human:
+                playerType = PlayerType.advanced_ai;
+                break;
+            case ai:
+                playerType = PlayerType.human;
+                break;
+            case advanced_ai:
+                playerType = PlayerType.ai;
+                break;
+        }
+        updatePlayerTypeText();
+    }
+    
+    /**
+     * Called when the right button is pressed and changes the player type
+     */
+    private void rightPressed()
+    {
+        switch (playerType)
+        {
+            case human:
+                playerType = PlayerType.ai;
+                break;
+            case ai:
+                playerType = PlayerType.advanced_ai;
+                break;
+            case advanced_ai:
+                playerType = PlayerType.human;
+                break;
+        }
+        updatePlayerTypeText();
     }
 }
---
+++ src/labyrinthboardgame/gui/PlayerSelectorBox.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labyrinthboardgame.gui;
+
+import javafx.geometry.Pos;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author Corbi
+ */
+public class PlayerSelectorBox extends VBox 
+{
+    private final PlayerSelector[] selectors;
+    
+    public PlayerSelectorBox()
+    {
+        super();
+        setAlignment(Pos.CENTER);
+        
+        selectors = new PlayerSelector[4];
+        selectors[0] = new PlayerSelector(Color.YELLOW);
+        selectors[1] = new PlayerSelector(Color.BLUE);
+        selectors[2] = new PlayerSelector(Color.GREEN);
+        selectors[3] = new PlayerSelector(Color.RED);
+        
+        for (int i = 0; i < 4; i++)
+        {
+            this.getChildren().add(selectors[i]);
+        }
+    }
+}
